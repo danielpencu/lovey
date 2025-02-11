1656d20e473b398eb2e3eb7788713d79aa84714c
danielpencu <penku.d102@gmail.com>
2025-02-11 15:55:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const yesBtn = document.getElementById("yes-btn");
 const noBtn = document.getElementById("no-btn");
 const gifContainer = document.getElementById("gif-container");
+const content = document.getElementById("content");
 
-let scale = 1; // Păstrează dimensiunea inițială
+let scale = 1; 
 
 noBtn.addEventListener("click", function() {
-    scale += 0.2; // Crește dimensiunea treptat
+    scale += 0.2; 
     yesBtn.style.transform = `scale(${scale})`;
 });
 
 yesBtn.addEventListener("click", function() {
-    gifContainer.classList.remove("hidden");
+    content.classList.add("hidden"); 
+    gifContainer.classList.remove("hidden"); 
 });
